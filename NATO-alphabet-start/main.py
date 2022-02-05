69ef23e14c2c0a21d53c56bbd3fc60f10e1b5360
Pliszka <karoliniakkjk@gmail.com>
2022-02-05 22:01:50 +0100

    except KeyError

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -3,10 +3,12 @@ import pandas
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
 words_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-word = input('Enter word:').upper()
-
-list_for_input = [words_dict[letter] for letter in word]
-
-print(list_for_input)
+word = input('Enter a word:').upper()
+try:
+    list_for_input = [words_dict[letter] for letter in word]
+except KeyError:
+    print('Please, enter only letters.')
+else:
+    print(list_for_input)
 
 
