e5cde47f30933a5782e29e9964497958ef2f3341
Pliszka <karoliniakkjk@gmail.com>
2022-02-05 22:07:42 +0100

    func spell added

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,14 +1,18 @@
 import pandas
 
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
-words_dict = {row.letter:row.code for (index, row) in data.iterrows()}
-
-word = input('Enter a word:').upper()
-try:
-    list_for_input = [words_dict[letter] for letter in word]
-except KeyError:
-    print('Please, enter only letters.')
-else:
-    print(list_for_input)
+words_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 
+def spell():
+    word = input('Enter a word:').upper()
+    try:
+        list_for_input = [words_dict[letter] for letter in word]
+    except KeyError:
+        print('Please, enter only letters.')
+        spell()
+    else:
+        print(list_for_input)
+
+
+spell()
